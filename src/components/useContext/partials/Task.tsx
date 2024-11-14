cbc91b2e22c99541a1ac53fc2e6d81b29d3d0eb3
Rafael Costa <leega_francisco.costa@slmandic.edu.br>
2024-11-14 12:50:13 -0300

    feat: useContext parte 5 utilizando o editTask

--- src/components/useContext/partials/Task.tsx
+++ src/components/useContext/partials/Task.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Task as TaskType } from "../../../store";
 import { TasksAction } from "../../../store/reducers";
 
@@ -9,13 +10,50 @@ interface TaskProps {
 }
 
 function Task({ task, onEditTask, onRemoveTask, dispatch }: TaskProps) {
-  return (
-    <>
-      {task.text}
-      <div className="flex gap-2">
-        <button className="rounded-full bg-green-500 px-5 py-2 text-white">
+  const [isEditing, setIsEditing] = useState(false);
+
+  let taskContent;
+
+  if (isEditing) {
+    taskContent = (
+      <>
+        <input
+          className="flex-1 rounded-full border border-gray-500 px-5 py-2"
+          type="text"
+          value={task.text}
+          onChange={(event) =>
+            onEditTask(dispatch, {
+              ...task,
+              text: event.target.value,
+            })
+          }
+        />
+        <button
+          onClick={() => setIsEditing(!isEditing)}
+          className="rounded-full bg-blue-500 px-5 py-2 text-white"
+        >
+          Salvar
+        </button>
+      </>
+    );
+  } else {
+    taskContent = (
+      <>
+        <div className="flex flex-1 items-center">{task.text}</div>
+        <button
+          onClick={() => setIsEditing(!isEditing)}
+          className="rounded-full bg-green-500 px-5 py-2 text-white"
+        >
           Editar
         </button>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div className="w-full flex gap-2">
+        {taskContent}
         <button
           onClick={() => onRemoveTask(dispatch, task.id)}
           className="rounded-full bg-red-500 px-5 py-2 text-white"
